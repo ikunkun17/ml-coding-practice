4f6991f9856cdf8b31b88da91582d02cadf0ae55
ikunkun17 <ikunkun5@gmail.com>
2026-03-22 20:11:20 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -29,9 +29,9 @@ print(sample_df['var_5'])
 print(sample_df[['var_1', 'var_4']])   # [ [] ]를 사용하면 [] 가 하나의 값으로 인식됨
 
 # loc 는 location의 앞글자
-print(sample_df.loc['a'])              # 행 기준으로 인덱싱
-print(sample_df.loc[['a','c','e']])
-print(sample_df.loc['b':'c'])
+print(sample_df.loc['b'])              # 행 기준으로 인덱싱
+print(sample_df.loc[['b','d','f']])
+print(sample_df.loc['b':'d'])
 
 # iloc: integer location의 약자
 print(sample_df.iloc[[0,1,2]])
