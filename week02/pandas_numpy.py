9411fb2ac3771840d60e958398e772bd51f52e50
ikunkun17 <ikunkun5@gmail.com>
2026-03-22 20:13:50 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -39,9 +39,9 @@ print(sample_df.iloc[0:3])
 print(sample_df.iloc[0:4])
 print(sample_df.iloc[0:4, 1:5])        # 컬럼까지 동시에 인덱싱
 
-print(sample_df.drop(['var_1','var_3'], axis=1))     # 컬럼을 제거하려면 axis = 1
-print(sample_df.drop(['var_1','var_2'], axis=1))
-print(sample_df.drop(['a','b','c'], axis=0))         # 행을 제거하려면 axis = 0 또는 디폴트로 사용
+print(sample_df.drop(['var_2','var_3'], axis=1))     # 컬럼을 제거하려면 axis = 1
+print(sample_df.drop(['var_2','var_3'], axis=1))
+print(sample_df.drop(['d','e','f'], axis=0))         # 행을 제거하려면 axis = 0 또는 디폴트로 사용
 
 netflix = pd.read_csv('2.1.1.netflix.csv')
 print(netflix.head())
