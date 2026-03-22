f635cb11f649efdc645780b28b9c6986195d44c8
ikunkun17 <ikunkun5@gmail.com>
2026-03-22 20:27:51 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -90,7 +90,7 @@ print(sample_df.reset_index())
 
 print(sample_df.reset_index(drop=True))          # 기존 인덱스는 제거하기
 
-print(sample_df.set_index('var_1'))
+print(sample_df.set_index('var_2'))
 
 
 print(sample_df.describe())
