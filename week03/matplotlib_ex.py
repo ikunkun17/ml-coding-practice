d40fb0c9ef152cd25f0b2b67f40a009570a611e9
ikunkun17 <ikunkun5@gmail.com>
2026-03-28 13:07:40 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -18,7 +18,7 @@ print(titanic.info())
 # 객실 등급에 따른 생존자와 사망자의 평균 계산
 pclass_survived_mean = titanic.groupby('Pclass')['Survived'].mean().reset_index()
 pclass_survived_mean
-w
+
 # 맷플롯립 라이브러리 불러오기
 import matplotlib.pyplot as plt
 
