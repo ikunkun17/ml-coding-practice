78144ab3240cf060fa953b23fbf98d8c02340938
ikunkun17 <ikunkun5@gmail.com>
2026-03-28 13:46:41 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -294,19 +294,19 @@ plt.figure(figsize=(10, 10))
 # 첫 번째 서브플롯 설정(선 그래프)
 plt.subplot(2, 1, 1)
 # 선 그래프 그리기
-plt.plot(x, y1, '-o', color='indigo', markersize=7, linewidth=3, alpha=0.7,
+plt.plot(x, y1, '-o', color='blue', markersize=7, linewidth=3, alpha=0.7,
 label='Not Survived')
 plt.xlabel('Parch')
-plt.ylabel('Not Survived Count', color='indigo')
-plt.tick_params(axis='y', labelcolor='indigo')
+plt.ylabel('Not Survived Count', color='blue')
+plt.tick_params(axis='y', labelcolor='blue')
 plt.legend(loc='upper right')
 
 # 두 번째 서브플롯 설정(막대 그래프)
 plt.subplot(2, 1, 2)
-plt.bar(x, y2, color='deeppink', alpha=0.8, width=0.8, label='Survived')
+plt.bar(x, y2, color='deepred', alpha=0.8, width=0.8, label='Survived')
 plt.xlabel('Parch')
-plt.ylabel('Survived Count', color='deeppink')
-plt.tick_params(axis='y', labelcolor='deeppink')
+plt.ylabel('Survived Count', color='deepred')
+plt.tick_params(axis='y', labelcolor='deepred')
 plt.legend(loc='upper right')
 
 # 제목 설정
