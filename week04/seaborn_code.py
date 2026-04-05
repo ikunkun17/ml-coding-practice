1acb877ce63a1e3299b33650f6723b447a4fad48
ikunkun17 <ikunkun5@gmail.com>
2026-04-05 20:49:04 +0900

    Updated Changes

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -31,7 +31,7 @@ plt.savefig('./week04/Seaborn_Figure01.jpg')
 
 # **빈도 그래프**
 # figure에 2개의 서브 플롯을 생성
-fig = plt.figure(figsize=(15, 5))
+fig = plt.figure(figsize=(16, 6))
 ax1 = fig.add_subplot(1, 2, 1)
 ax2 = fig.add_subplot(1, 2, 2)
 
