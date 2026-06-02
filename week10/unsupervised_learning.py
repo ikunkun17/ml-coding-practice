c042f5238ff982f5e937e84e9a5b30836e776976
ikunkun17 <ikunkun5@gmail.com>
2026-06-02 19:44:13 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -49,7 +49,7 @@ print(kmeans.inertia_)
 
 print(kmeans.score(X))
 
-kmeans_per_k = [KMeans(n_clusters=k, n_init=10, random_state=42).fit(X) for k in range(1, 10)]
+kmeans_per_k = [KMeans(n_clusters=k, n_init=12, random_state=43).fit(X) for k in range(1, 10)]
 inertias = [model.inertia_ for model in kmeans_per_k]
 
 plt.figure(figsize=(8, 3.5))
